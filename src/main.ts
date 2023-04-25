5008527ca7eb5d32315e805b28513bcfd428e44e
mehunk <mehunk@163.com>
2023-04-26 00:17:22 +0900

    chore: protect the app by adding helmet

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -6,6 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();

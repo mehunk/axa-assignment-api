02151d52811246f97689ce98039de7c67a69ca5c
mehunk <mehunk@163.com>
2023-04-26 16:21:42 +0900

    feat: add getProduct api

--- src/vehicle-types/vehicle-types.controller.spec.ts
+++ src/vehicle-types/vehicle-types.controller.spec.ts
@@ -3,8 +3,29 @@ import { MockFunctionMetadata, ModuleMocker } from 'jest-mock';
 
 import { VehicleTypesController } from './vehicle-types.controller';
 import { VehicleTypesService } from './vehicle-types.service';
+import { ProductsService } from '@/products/products.service';
 
 const moduleMocker = new ModuleMocker(global);
+const vehicleTypes = [
+  {
+    id: 1,
+    name: 'Product 1',
+    description: 'Product 1 description',
+    status: 1,
+  },
+];
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    description: 'Product 1 description',
+    status: 1,
+    deductible: 100,
+    premium: 100,
+    policyLimit: 100,
+    vehicleTypeId: 1,
+  },
+];
 
 describe('VehicleTypesController', () => {
   let controller: VehicleTypesController;
@@ -14,16 +35,17 @@ describe('VehicleTypesController', () => {
       controllers: [VehicleTypesController],
     })
       .useMocker((token) => {
-        const results = [
-          {
-            id: 1,
-            name: 'Product 1',
-            description: 'Product 1 description',
-            status: 1,
-          },
-        ];
         if (token === VehicleTypesService) {
-          return { findAll: jest.fn().mockResolvedValue(results) };
+          return {
+            findAll: jest.fn().mockResolvedValue(vehicleTypes),
+            findOne: jest.fn().mockResolvedValue(vehicleTypes[0]),
+          };
+        }
+        if (token === ProductsService) {
+          return {
+            findProducts: jest.fn().mockResolvedValue(products),
+            findOneByVehicleTypeId: jest.fn().mockResolvedValue(products[0]),
+          };
         }
         if (typeof token === 'function') {
           const mockMetadata = moduleMocker.getMetadata(
@@ -50,6 +72,32 @@ describe('VehicleTypesController', () => {
         name: 'Product 1',
         description: 'Product 1 description',
         status: 1,
+      },
+    ]);
+  });
+
+  it('should return a vehicle-type', async () => {
+    const product = await controller.findOne(1);
+    expect(product).toEqual({
+      id: 1,
+      name: 'Product 1',
+      description: 'Product 1 description',
+      status: 1,
+    });
+  });
+
+  it('should return an array of products', async () => {
+    const products = await controller.findProducts(1);
+    expect(products).toEqual([
+      {
+        id: 1,
+        name: 'Product 1',
+        description: 'Product 1 description',
+        status: 1,
+        deductible: 100,
+        premium: 100,
+        policyLimit: 100,
+        vehicleTypeId: 1,
       },
     ]);
   });
--- src/vehicle-types/vehicle-types.controller.ts
+++ src/vehicle-types/vehicle-types.controller.ts
@@ -6,22 +6,30 @@ import { ProductsService } from '@/products/products.service';
 @Controller('vehicle-types')
 export class VehicleTypesController {
   constructor(
-    private readonly productsService: VehicleTypesService,
-    private readonly productOptionsService: ProductsService,
+    private readonly vehicleTypesService: VehicleTypesService,
+    private readonly productsService: ProductsService,
   ) {}
 
   @Get()
   findAll() {
-    return this.productsService.findAll();
+    return this.vehicleTypesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.productsService.findOne(id);
+    return this.vehicleTypesService.findOne(id);
   }
 
   @Get(':id/products')
-  findOptions(@Param('id', ParseIntPipe) id: number) {
-    return this.productOptionsService.findProducts(id);
+  findProducts(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findProducts(id);
+  }
+
+  @Get(':id/products/:productId')
+  findProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('productId', ParseIntPipe) productId: number,
+  ) {
+    return this.productsService.findOneByVehicleTypeId(id, productId);
   }
 }
